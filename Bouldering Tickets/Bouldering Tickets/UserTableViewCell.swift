3d5018128b3f79cf238a65e00d88ee3bf7128509
Sergey Pisarchik <Sergey_pisarchik@tut.by>
2015-09-13 16:47:08 +0300

    Добавил таблицу и ячейку

---
+++ Bouldering Tickets/Bouldering Tickets/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableViewCell.swift
+//  Bouldering Tickets
+//
+//  Created by Sergey Pisarchik on 9/13/15.
+//  Copyright (c) 2015 Sergey Pisarchik. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
