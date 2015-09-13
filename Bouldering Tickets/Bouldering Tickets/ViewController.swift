564c6695ddeec62e4651d0a2b44ef1d86bd4d5bc
Sergey Pisarchik <Sergey_pisarchik@tut.by>
2015-09-13 10:29:55 +0300

    Добавил прятание клавиатуры при нажатии на картинку

--- Bouldering Tickets/Bouldering Tickets/ViewController.swift
+++ Bouldering Tickets/Bouldering Tickets/ViewController.swift
@@ -22,22 +22,71 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     // MARK: Properties
     @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var secondName: UITextField!
+    @IBOutlet weak var fathersName: UITextField!
+    @IBOutlet weak var Phone: UITextField!
+    @IBOutlet weak var mail: UITextField!
+    @IBOutlet weak var enterSource: UITextField!
+    @IBOutlet weak var discount: UITextField!
+    @IBOutlet weak var note: UITextView!
 
 
     // MARK: Actions
-    @IBAction func takeAPhoto(sender: UITapGestureRecognizer) {
-        
+    @IBAction func takePicture(sender: UILongPressGestureRecognizer) {
         
         // UIImagePickerController is a view controller that lets a user pick media from their photo library.
         let imagePickerController = UIImagePickerController()
         
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            imagePickerController.sourceType = .Camera
+        }
+        else {
+            imagePickerController.sourceType = .PhotoLibrary
+        }
         // Only allow photos to be picked, not taken.
-        imagePickerController.sourceType = .PhotoLibrary
         
         // Make sure ViewController is notified when the user picks an image.
         imagePickerController.delegate = self
         
         presentViewController(imagePickerController, animated: true, completion: nil)
     }
+    @IBAction func showPhoto(sender: UITapGestureRecognizer) {
+        HideKeyboard()
+        
+        
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+            // The info dictionary contains multiple representations of the image, and this uses the original.
+            let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            
+            // Set photoImageView to display the selected image.
+            photo.image = selectedImage
+            
+            // Dismiss the picker.
+            dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: methods
+    func HideKeyboard(){
+        firstName.resignFirstResponder()
+        secondName.resignFirstResponder()
+        fathersName.resignFirstResponder()
+        Phone.resignFirstResponder()
+        mail.resignFirstResponder()
+        enterSource.resignFirstResponder()
+        discount.resignFirstResponder()
+        note.resignFirstResponder()
+        
+    }
+    
 }
 
