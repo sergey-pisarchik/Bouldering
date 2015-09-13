f792704f08cdb20733ba636fe9d06c728cd155e3
Sergey Pisarchik <Sergey_pisarchik@tut.by>
2015-09-13 08:33:06 +0300

    [IB] Расположил элементы на экране с прокруткой

---
+++ Bouldering Tickets/Bouldering Tickets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bouldering Tickets
+//
+//  Created by Sergey Pisarchik on 9/12/15.
+//  Copyright (c) 2015 Sergey Pisarchik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
